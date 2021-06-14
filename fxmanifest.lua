f40947206ec75b9a33f4b7e2f94728a5d58701a3
sync <50116007+synceye@users.noreply.github.com>
2021-06-14 21:19:36 +0200

    needed for installer

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+name 'Mythic Framework Notification System'
+author 'Alzar - https://github.com/Alzar'
+version 'v1.1.0'
+
+ui_page 'html/ui.html'
+
+files {
+	'html/ui.html',
+	'html/js/app.js', 
+	'html/css/style.css',
+}
+
+client_scripts 'client/main.lua'
+
+exports {
+	'SendAlert',
+	'SendUniqueAlert',
+	'PersistentAlert',
+}
